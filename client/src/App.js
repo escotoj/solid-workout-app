05b8ea734e7b36507d425e11a63471c36e3e3456
escotoj <j.escoto14@gmail.com>
2023-07-09 13:26:00 -0700

    add components and src server running

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,79 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, TextField, Button, Paper } from '@material-ui/core';
+import Header from './components/Header'
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    padding: theme.spacing(3),
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  input: {
+    marginRight: theme.spacing(2),
+  },
+  submitButton: {
+    marginLeft: theme.spacing(2),
+  },
+  submittedValue: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [inputValue, setInputValue] = useState('');
+  const [submittedValue, setSubmittedValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedValue(inputValue);
+    setInputValue('');
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div>
-SOLID WORKOUT
+    <div className={classes.root}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Solid Workout App
+      </Typography>
+      <Header />
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          className={classes.input}
+          variant="outlined"
+          label="Log Workout"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button
+            className={classes.submitButton}
+            variant="contained"
+            color="success"
+            type="submit"
+            sx={{ backgroundColor: 'black' }}
+        >
+          Log
+        </Button>
+      </form>
+      {submittedValue && (
+        <Paper className={classes.submittedValue}>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Workout:
+        </Typography>
+        <Typography variant="body1">{submittedValue}</Typography>
+      </Paper>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import NavBar from "./NavBar";
+
+const Header = () => {
+  return (
+ 
+    <Grid bgcolor="lavenderblush" spacing={1} textAlign="center" container={true} >
+      <Grid item xs={12}>
+      <Typography variant="h2" component="h2" style={{ fontWeight: '400' }}>
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <NavBar />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Header;
