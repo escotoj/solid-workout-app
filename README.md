8926533cff62253fbce8ee5480e26b2f7afaa9cf
escotoj <j.escoto14@gmail.com>
2023-07-17 00:49:18 -0700

    add link to 'solid workout' to the '/' path

--- README.md
+++ README.md
@@ -29,6 +29,8 @@ GitHub: [escotoj](https://github.com/escotoj)
 
 ## TODO 
 
+- add link to header to take you back to '/'
+- work on the user models for longin and sing up
 - create feeds page that will display the workouts like a blog.
 - Workouts Page - API can be made needs its own page. 
 - need to set up, the entire user model and workout models and link to the schemas.
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,15 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
 import NavBar from "./NavBar";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
  
     <Grid bgcolor="lavenderblush" spacing={1} textAlign="center" container={true} >
       <Grid item xs={12}>
-      <Typography variant="h2" component="h2" style={{ fontWeight: '400' }}>Solid Workout
+      <Typography variant="h2" component={Link} to="/" style={{ fontWeight: '400', textDecoration: 'none', color: 'inherit' }}>
+          Solid Workout
         </Typography>
       </Grid>
       <Grid item xs={12}>
