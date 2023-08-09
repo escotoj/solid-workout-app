d955e1b2c13dda55cdb35b7b887c16a2e81fe28b
escotoj <j.escoto14@gmail.com>
2023-08-08 20:40:25 -0700

    add cards to the home page, need to add photos and links

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 const useStyles = makeStyles((theme) => ({
   hero: {
-    // backgroundImage: 'url("/path/to/your/background-image.jpg")',
+    // backgroundImage: 'url("")',
     // backgroundSize: 'cover',
     textAlign: "center",
     padding: theme.spacing(1),
   },
   content: {
-    textAlign: 'center',
+    textAlign: "center",
     padding: theme.spacing(4),
   },
   button: {
@@ -32,28 +37,28 @@ const LandingPage = () => {
         <Container>
           <Grid justifyContent="center">
             <Grid item xs={12} md={8}>
-            <Typography
-        variant="h3"
-        component="h1"
-        gutterBottom
-        sx={{
-          fontWeight: 'bold', 
-          color: '#00000', 
-        }}
-      >
-        Welcome to
-      </Typography>
-      <Typography
-        variant="h3"
-        component="h1"
-        gutterBottom
-        sx={{
-          fontStyle: 'italic', // Apply italic font style
-          color: '#00000', // Change the text color
-        }}
-      >
-      Solid Workout
-      </Typography>
+              <Typography
+                variant="h3"
+                component="h1"
+                gutterBottom
+                sx={{
+                  fontWeight: "bold",
+                  color: "#00000",
+                }}
+              >
+                Welcome to
+              </Typography>
+              <Typography
+                variant="h3"
+                component="h1"
+                gutterBottom
+                sx={{
+                  fontStyle: "italic",
+                  color: "#00000",
+                }}
+              >
+                Solid Workout
+              </Typography>
               <Typography variant="h6" color="textSecondary" paragraph>
                 Your go-to platform for achieving your fitness and health gains.
               </Typography>
@@ -77,12 +82,59 @@ const LandingPage = () => {
         <Typography variant="body1" align="center" paragraph>
           Explore the features that will help you on your fitness journey.
         </Typography>
-        <Typography variant="body1" align="center" paragraph>
-          Exercise Search
-        </Typography>
-        <Typography variant="body1" align="center" paragraph>
-        Log Your Gains
-        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              Exercise Search
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+This feature allows you create a search for exercises based on target muscles. 
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "30vh",
+          }}
+        >
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Log Your Gains
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Feature that allows you to keep track of your workout history. 
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Box>
       </Container>
     </div>
   );
