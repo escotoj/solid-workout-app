32a96da2514fab175f798789b851e8a4e601af82
escotoj <j.escoto14@gmail.com>
2023-08-21 16:29:28 -0700

    update to import name in exercises page part 32

--- client/src/pages/Exercises.js
+++ client/src/pages/Exercises.js
@@ -13,7 +13,7 @@ import {
   DialogContent,
   DialogActions,
 } from "@mui/material";
-import UnsplashFitnessGallery from "../components/FitnessPhotos.js";
+import UnsplashFitnessGallery from "../components/fitnessPhotos.js";
 
 
 const Exercise = () => {
